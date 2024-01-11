e80f5fb0b38aba9cf348b8d898b31db048ae6b5e
Guilherme Matos <guilherme.thrmatos@gmail.com>
2024-01-10 21:47:08 -0300

    Adding another page with BarbaJS

--- assets/js/app.js
+++ assets/js/app.js
@@ -64,7 +64,6 @@ function animateSlides() {
 const mouse = document.querySelector(".cursor");
 const mouseTxt = mouse.querySelector("span");
 const burger = document.querySelector(".burger");
-console.log(burger);
 function cursor(e) {
   mouse.style.top = e.pageY + "px";
   mouse.style.left = e.pageX + "px";
